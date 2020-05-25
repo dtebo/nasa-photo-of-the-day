f4fdb6a698b8edc6c05ed29c6bdee84febea72ba
Darren Tebo <darrentebo83@outlook.com>
2020-05-24 23:59:28 -0400

    began building components

---
+++ src/Components/Header/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div>
+            <span className="title">APOD</span>
+        </div>
+    );
+};
+
+export default Title;
