21337ad720795e91278c032a7f565d178c0aeba7
Darren Tebo <darrentebo83@outlook.com>
2020-05-26 14:38:28 -0400

    began adding api call function for the data from NASA

--- src/Components/PhotoPage/PhotoContainer.js
+++ src/Components/PhotoPage/PhotoContainer.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Carousel from './Carousel';
 
 const PhotoContainer = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Get our data
+    }, []);
+
     return (
         <div className="photo-container">
             <Carousel />
